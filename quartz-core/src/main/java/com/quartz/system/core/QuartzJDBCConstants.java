024999fc8ff71a9743ceb81c299a2736c717cea0
gaowei <gao_wei0115@sina.com>
2018-11-29 13:29:43 +0800

    init project

---
+++ quartz-core/src/main/java/com/quartz/system/core/QuartzJDBCConstants.java
@@ -0,0 +1,15 @@
+package com.quartz.system.core;
+
+import org.quartz.impl.jdbcjobstore.Constants;
+
+/**
+ * @packageName：com.quartz.system.core
+ * @desrciption: 复写sql操作常量
+ * @author: gaowei
+ * @date： 2018-11-29 13:28
+ * @history: (version) author date desc
+ */
+public interface QuartzJDBCConstants extends Constants {
+
+
+}
